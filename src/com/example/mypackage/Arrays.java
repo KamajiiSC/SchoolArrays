ce38c9abb199cc967607f4637a94d13e8dfc21bd
KamajiiSC <brandonlschaen@gmail.com>
2019-09-02 17:04:55 -0400

    Added prompt to enter 0-50

--- src/com/example/mypackage/Arrays.java
+++ src/com/example/mypackage/Arrays.java
@@ -10,6 +10,8 @@ public class Arrays {
 		
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Please enter a number between 0-50: ");
+		
 		//Get Input
 		for(int i = 0;  i < input.length; i++) {
 			System.out.println("Enter number " + (i +1 ) + " : ");
@@ -19,6 +21,13 @@ public class Arrays {
 		//Increment counter array indexes corresponding to input
 		for(int i = 0; i< input.length; i++) {
 			counter[input[i]]++;
+		}
+		
+		//Print stored values
+		for(int i = 0; i< counter.length; i++) {
+			if((counter[i] > 0)) {
+				System.out.println(i + ": " + counter[i]);
+			}
 		}
 	}
 
