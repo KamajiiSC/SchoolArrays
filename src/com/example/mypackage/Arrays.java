a9b133549ab94c07b53494b8e1abf2a659bde6c4
KamajiiSC <brandonlschaen@gmail.com>
2019-09-02 16:17:59 -0400

    Imported scanner and created scan object

--- src/com/example/mypackage/Arrays.java
+++ src/com/example/mypackage/Arrays.java
@@ -1,10 +1,14 @@
 package com.example.mypackage;
 
+import java.util.Scanner;
+
 public class Arrays {
 
 	public static void main(String[] args) {
 		int[] counter = new int[51];
 		int[] input = new int[10];
+		
+		Scanner scan = new Scanner(System.in);
 	}
 
 }
