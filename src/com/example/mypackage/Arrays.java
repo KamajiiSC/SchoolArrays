3b187d6d543b7e0998b5ac01a0be231bf2cea41c
KamajiiSC <brandonlschaen@gmail.com>
2019-09-02 16:25:23 -0400

    Made the input for the array

--- src/com/example/mypackage/Arrays.java
+++ src/com/example/mypackage/Arrays.java
@@ -11,7 +11,8 @@ public class Arrays {
 		Scanner scan = new Scanner(System.in);
 		
 		for(int i = 0;  i < input.length; i++) {
-			
+			System.out.println("Enter 10 numbers: ");
+			input[i] = scan.nextInt();
 		}
 	}
 
